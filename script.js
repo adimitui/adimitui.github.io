f276b2fef5dc12d0794ad032bb99979a6be51b8f
ADELLE INGRID DIMITUI <adimitui@princeton.edu>
2017-08-02 05:16:07 -0400

    added animation to skills section

--- script.js
+++ script.js
@@ -25,4 +25,44 @@ app.controller('NavbarCtrl', function($scope) {
 });
 
 app.controller('HomeCtrl', function($scope) {
+	$(window).scroll(function() {
+		if ($(window).scrollTop() + $(window).height() >= $('#skills-software-graph').offset().top) {
+			softwareSkills = $('.skills-software');
+			for (var i = 0; i < softwareSkills.length; i++) {
+				var temp = softwareSkills[i].id;
+				var software = $('#' + temp);
+				var softwareId = temp.slice(0, 2);
+				animateSkillsGraph(softwareId, software);
+			}
+		}
+
+		if ($(window).scrollTop() + $(window).height() >= $('#skills-languages-graph').offset().top) {
+			languageSkills = $('.skills-languages');
+			for (var i = 0; i < languageSkills.length; i++) {
+				var temp = languageSkills[i].id;
+				var language = $('#' + temp);
+				var languageId = temp.slice(0, 2);
+				animateSkillsGraph(languageId, language);
+			}
+		}
+	})
+
+	function animateSkillsGraph(condition, element) {
+		switch (condition) {
+			case '20':
+				element.addClass('skills__graph__columns__row__level--20');
+				break;
+			case '40':
+				element.addClass('skills__graph__columns__row__level--40');
+				break;
+			case '60':
+				element.addClass('skills__graph__columns__row__level--60');
+				break;
+			case '80':
+				element.addClass('skills__graph__columns__row__level--80');
+				break;
+			default:
+				element.addClass('skills__graph__columns__row__level--100');
+		}
+	}
 });
